826f930dd2fd52dcd8ae362dcc17e6e8768305c8
samhk90 <mdsami9022@gmail.com>
2023-08-09 10:18:09 +0530

    fixed bug at genetic algorithm

--- classmaster/views.py
+++ classmaster/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .genetic_algorithm import initialize_population, fitness_function, selection, crossover, mutation, genetic_algorithm
+from genetic_algorithm import initialize_population, fitness_function, selection, crossover, mutation, genetic_algorithm
 from .models import Faculty, Subject  # Import your models
 
 # Create your views here.
